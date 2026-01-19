02c59f86a23cafb1c2be97438455cf924608c67e
hill <hill.houseofpy@gmail.com>
2026-01-19 18:38:32 +0530

    add the logic of unique bill number

--- jal_crm/models/inherit_stock.py
+++ jal_crm/models/inherit_stock.py
@@ -8,3 +8,20 @@ class InheriteStockPicking(models.Model):
     vendor_bill_no = fields.Char(string="Vendor Bill No.")
     vendor_bill_date = fields.Date(string="Vendor Bill Date")
     vehicle_no = fields.Char(string="Vehicle No")
+
+    @api.onchange('vendor_bill_no')
+    def _onchange_vendor_bill_no(self):
+        if self.vendor_bill_no:
+            domain = [
+                ('partner_id', '=', self.partner_id.id),
+                ('vendor_bill_no', '=', self.vendor_bill_no),
+            ]
+
+            if self.env.user.fy_year_id:
+                domain.append(('vendor_bill_date', '>=', self.env.user.fy_from_date))
+                domain.append(('vendor_bill_date', '<=', self.env.user.fy_to_date))
+
+            picking_rec = self.env['stock.picking'].search(domain)
+
+            if picking_rec:
+                raise UserError(_("This Vendor Bill Number is already generated for this Vendor"))
